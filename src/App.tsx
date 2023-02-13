6df9e1176fec57ff1312e5c6f39f2f9d048729c7
Mschroeder95 <95.michael.schroeder@gmail.com>
2023-02-12 22:34:04 -0600

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Route, Link, Routes } from 'react-router-dom'
 
+function Stub() {
+  return <h2>stub</h2>;
+}
+ 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+       <div>
+         <nav>
+           <ul>
+             <li>
+               <Link to="/test">stub</Link>
+             </li>
+           </ul>
+         </nav>
+        <Routes>
+          <Route path="/test" element={<Stub />} />
+        </Routes>
+       </div>
+      </Router>
     </div>
   );
 }
